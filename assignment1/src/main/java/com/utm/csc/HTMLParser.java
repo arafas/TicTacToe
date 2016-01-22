baab31a2c2eb1f7fef1af97b4a0118d4fe449550
Syeda Arafa Sulaiman <syedaarafasulaiman@Syedas-MacBook-Air.local>
2016-01-21 23:15:26 -0500

    Committing changes to existing files :wq

--- assignment1/src/main/java/com/utm/csc/HTMLParser.java
+++ assignment1/src/main/java/com/utm/csc/HTMLParser.java
@@ -1,7 +1,73 @@
 package com.utm.csc;
-public class HTMLParser {
 
-	public static void main(String[] args) {
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class HTMLParser {
+	
+	
+	public static void parseHTML(String url_page) throws IOException{
 		
+		System.out.println("URL : " + url_page);
+		Document doc = Jsoup.connect(url_page).get();
+
+		
+		String page_title = doc.title();
+		System.out.println("title : " + page_title);
+		System.out.println("");
+	
+		Elements links = doc.getElementsByTag("a");
+		Integer count_line = 0;
+		for (Element link : links) {
+
+		  String actualLink = link.attr("href");
+		  String linkText = link.text();
+		  if (actualLink != ""){
+			  count_line = count_line + 1;
+			  System.out.println("link : " + actualLink);
+			  System.out.println("text : " + linkText);
+			  System.out.println("");
+		  }
+		}
+		System.out.println("Link Count : " + count_line);
+		
+	}
+
+	public static void main(String[] args) throws IOException {
+		
+		
+		for(int i=0; i<args.length; i++){
+			String url_page = args[i];
+			System.out.println("");
+			parseHTML(url_page);
+		}
+		/*
+		System.out.println(url_page + " testing dis shit");
+		Document doc = Jsoup.connect(url_page).get();
+
+		
+		String page_title = doc.title();
+		System.out.println("title : " + page_title);
+		System.out.println("");
+		
+		//Element content = doc.getElementById("content");
+		Elements links = doc.getElementsByTag("a");
+		Integer count_line = 0;
+		for (Element link : links) {
+
+		  String actualLink = link.attr("href");
+		  String linkText = link.text();
+		  if (actualLink != ""){
+			  count_line = count_line + 1;
+			  System.out.println("link : " + actualLink);
+			  System.out.println("text : " + linkText);
+			  System.out.println("");
+		  }
+		}
+		System.out.println(count_line);*/
 	}
 }
